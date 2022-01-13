87c67e31014acf84f4ae9adf12775f875f58438f
chrisnieves60 <chrisnieves60@gmail.com>
2022-01-12 21:27:23 -0500

    assignment-7

--- src/App.js
+++ src/App.js
@@ -11,24 +11,6 @@ function App() {
   const [trend, setTrend] = useState([]) 
   const [search, setSearch] = useState([]);  //used to set search
   //create third use state to handle object data
-
-
-  let trendURL = `http://api.giphy.com/v1/gifs/trending?api_key=59AiF3Hk64GO1i7avk4ycS6LY9uz0576`//only shows trending for now 
-  const getTrend = async () => { 
-    await axios.get(trendURL) //await for axios to get url 
-
-      .then(res => { //after await,  
-        setTrend(res.data.data) //set trending, DATA IS NESTED TWICE 
-        console.log(res.data.data) 
-      })
-
-       
-    }
-
-
-  useEffect(() => { //run getTrend function, when state changes, rerender getTrend
-    getTrend()
-  }, [])
   
   return (
     <div>
@@ -36,17 +18,10 @@ function App() {
         <div className="text-light text-center"><b>GIPHY Search</b></div>
       </div>
 
-      
-      
       <div className="container d-flex justify-content-center">
       <SearchField searchFromApp={search} setSearchFromApp={setSearch}/>
         </div>
-        
-        {
-          trend.map(e => { //create new array e 
-            return <img key={e.key} className='col-3' src={e.images.original.url} alt='GIPHY Results'></img> //give each outputted gif a key e.id and src output e.images.original.url
-          })
-        }
+        <GifCard trendFromApp={trend} setTrendFromApp={setTrend}/>
     </div >
     
   )
--- src/Components/GifCard.js
+++ src/Components/GifCard.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import axios from 'axios';
+import { Fragment, useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-//GifCard Component: presentational component receiving GIF info as props
+function GifCard(props) { //perhaps show trending by default? 
+    const {trendFromApp, setTrendFromApp} = props;
 
-function GifCard (trending) { //perhaps show trending by default? 
-    
+    let trendURL = `http://api.giphy.com/v1/gifs/trending?api_key=59AiF3Hk64GO1i7avk4ycS6LY9uz0576`//only shows trending for now 
+  const getTrend = async () => { 
+    await axios.get(trendURL) //await for axios to get url 
+
+      .then(res => { //after await,  
+        setTrendFromApp(res.data.data) //set trending, DATA IS NESTED TWICE 
+        console.log(res.data.data) 
+      })
+
+       
+    }
+    useEffect(() => { //run getTrend function, when state changes, rerender getTrend
+        getTrend()
+      }, [])
+
+      return (
+      <Fragment>
+
+       { trendFromApp.map(e => { //create new array e 
+      return <img key={e.key} className='col-3' src={e.images.original.url} alt='GIPHY Results'></img>})
+      }
+
+      
+
+      </Fragment>
+      )
 
 }
 
