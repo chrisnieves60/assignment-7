59157944650e1bd5480cf185bde6af873ebd83ee
chrisnieves60 <chrisnieves60@gmail.com>
2022-01-17 14:28:15 -0500

    final stage

--- src/App.js
+++ src/App.js
@@ -3,15 +3,58 @@ import ReactDOM, { findDOMNode, flushSync } from 'react-dom';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import GifCard from './Components/GifCard';
-import SearchField from './Components/SearchField';
+
 
 
 function App() {
 
-  const [trend, setTrend] = useState([]) 
-  const [search, setSearch] = useState([]);  //used to set search
-  //create third use state to handle object data
+  const [gifs, setGifs] = useState([]) 
+  const [search, setSearch] = useState([]);  //recieve prop info as search
   
+  let trendURL = `http://api.giphy.com/v1/gifs/trending?api_key=59AiF3Hk64GO1i7avk4ycS6LY9uz0576`//only shows trending BY DEFAULT 
+  const getGifs = async () => { 
+    await axios.get(trendURL) //await for axios to get url 
+
+      .then(res => { //after await,  
+        setGifs(res.data.data) //set gifs to trending, DATA IS NESTED TWICE 
+        console.log(res.data.data) 
+      })
+    }
+
+    
+//---------
+const HandleChange = (event) => {
+  setSearch(event.target.value); //set search in realtime by getting event.target.value
+}
+
+const HandleSubmit = async () => { //on submit
+  if (search.includes(' ')){
+    setSearch(search.replace(' ', '+')) //do this to add + between url spaces
+  }
+  let searchURL = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=59AiF3Hk64GO1i7avk4ycS6LY9uz0576`; //backtick around it to update url
+  await axios.get(searchURL) //await for axios to get url 
+
+  .then(res => { //after await,  
+    setGifs(res.data.data) //set searchfromapp to
+    console.log(res.data.data)
+  })
+}
+
+const HandleRand = async () => { //on submit
+  
+  let searchURL = `http://api.giphy.com/v1/gifs/random?api_key=59AiF3Hk64GO1i7avk4ycS6LY9uz0576`;
+  await axios.get(searchURL) //await for axios to get url 
+
+  .then(res => { //after await,  
+    setGifs(new Array(res.data.data)) //set searchfromapp to
+    console.log(res)
+  })
+
+}
+useEffect(() => { //run getGifs function, when state changes, rerender getGifs
+  getGifs()
+}, [])
+
   return (
     <div>
       <div className="p-5 bg-dark fs-1">
@@ -19,9 +62,12 @@ function App() {
       </div>
 
       <div className="container d-flex justify-content-center">
-      <SearchField searchFromApp={search} setSearchFromApp={setSearch}/>
+            <input className='search' onChange={HandleChange}></input> 
+            <button onClick={HandleSubmit}>Search</button>
+            <button onClick={HandleRand}>Random</button>
         </div>
-        <GifCard trendFromApp={trend} setTrendFromApp={setTrend}/>
+        <GifCard gifsFromApp={gifs} setGifsFromApp={setGifs}/>
+        
     </div >
     
   )
--- src/Components/GifCard.js
+++ src/Components/GifCard.js
@@ -4,36 +4,16 @@ import { Fragment, useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function GifCard(props) { //perhaps show trending by default? 
-    const {trendFromApp, setTrendFromApp} = props;
-
-    let trendURL = `http://api.giphy.com/v1/gifs/trending?api_key=59AiF3Hk64GO1i7avk4ycS6LY9uz0576`//only shows trending for now 
-  const getTrend = async () => { 
-    await axios.get(trendURL) //await for axios to get url 
-
-      .then(res => { //after await,  
-        setTrendFromApp(res.data.data) //set trending, DATA IS NESTED TWICE 
-        console.log(res.data.data) 
-      })
-
-       
-    }
-    useEffect(() => { //run getTrend function, when state changes, rerender getTrend
-        getTrend()
-      }, [])
+function GifCard(props) { 
+    const {gifsFromApp, setGifsFromApp}= props; //we can have setGifs update WHEN SEARCHFIELD 
 
       return (
       <Fragment>
-
-       { trendFromApp.map(e => { //create new array e 
+       { gifsFromApp.map(e => { //create new array e 
       return <img key={e.key} className='col-3' src={e.images.original.url} alt='GIPHY Results'></img>})
       }
-
-      
-
       </Fragment>
       )
-
 }
 
 export default GifCard
